7ceb4a2aefed95a04d4799d92030e40aa088ca95
James_prsr <james.prsr21@gmail.com>
2024-11-05 11:03:51 +0530

    Update amba_notes.sv

--- amba_notes.sv
+++ amba_notes.sv
@@ -1,5 +1,6 @@
-Protocol are nothing but set of rules by which we can communicate between master and slave
+Protocol are nothing but set of rules by which we can communicate between master and slave.
 There are two types of Protocol. 1> Onchip protocol ; 2>Offchip/Peripheral Protocol.
+The SoC has several functional blocks that use AMBA protocols like AXI4 and AXI3 to communicate with each other.
 
 AMBA stands for Advanced Microcontroller Bus Architecture
   It is a onchip protocol. Which is used to connect functional blocks which are on the chip(SOC).
