5e77ebe1ab39cef58f858701b9046c66ba676c0f
James_prsr <james.prsr21@gmail.com>
2024-11-05 09:54:00 +0530

    Update axi_notes.sv

--- AXI/axi_notes.sv
+++ AXI/axi_notes.sv
@@ -1,1 +1,3 @@
-
+AXI stands for Advanced eXtensible Interface.
+  It is a high-performance, parallel bus that connects processor cores to on-chip peripheral circuits.
+  A high-performance, point-to-point, master-slave parallel bus that connects processor cores to on-chip peripheral circuits
