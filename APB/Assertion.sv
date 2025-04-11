5321b9f81ca04f40d6a34f569e45d10ea96e8ba1
James_prsr <james.prsr21@gmail.com>
2025-04-11 03:31:42 -0400

    Update Assertion.sv

--- APB/Assertion.sv
+++ APB/Assertion.sv
@@ -1,1 +1,21 @@
+module apb_assertion(input bit pwrite, bit[31:0]pwdata, bit[31:0]paddr, bit pready, bit[31:0]prdata, bit presetn, bit psel, bit enable, bit pclk);
 
+  //Reset checking- after reset all signals should in default state.
+  property p0;
+    @(posedge pclk)
+    (!presetn) |-> (psel==1'b0 && penable==1'b0 && pready==1'b0 && paddr=='b0 && pwdata=='b0 && prdata=='b0 && pslverr==1'b0);
+  endproperty
+
+  //Handshaking - when penable is high and from the same posedge pready gets high for one cylce, then in the next cyce penable and pready become blow.
+  property p1;
+    @(posedge pclk) disable iff(!presetn) 
+    $rose(penable) |-> pready[->1] ##1 $fell(penable) && $fell(pready); 
+  endproperty
+
+  //Write operation
+    property p2;
+    @(posedge pclk) disable iff(!presetn) 
+      $rose(pwrite && psel) |-> $stable(paddr && pwdata) |=> $rose(penable) |-> ##[0:15]$rose(pready) ##1 $fell(psel && pwrite && penable && pready);   
+  endproperty
+
+  
