2513a19b732624d2cfbde6e60dc4759cf9a94c95
James_prsr <james.prsr21@gmail.com>
2025-04-11 06:37:51 -0400

    Update Assertion.sv

--- APB/Assertion.sv
+++ APB/Assertion.sv
@@ -18,4 +18,50 @@ module apb_assertion(input bit pwrite, bit[31:0]pwdata, bit[31:0]paddr, bit prea
       $rose(pwrite && psel) |-> $stable(paddr && pwdata) |=> $rose(penable) |-> ##[0:15]$rose(pready) ##1 $fell(psel && pwrite && penable && pready);   
   endproperty
 
+    //Read operation
+    property p2;
+    @(posedge pclk) disable iff(!presetn) 
+      (!pwrite && psel) |-> $stable(paddr && prdata) |=> $rose(penable) |-> ##[0:15]$rose(pready) ##1 $fell(psel && pwrite && penable && pready);   
+  endproperty
+
+  property p3;
+    @(posedge pclk) disable iff(!presetn)
+    $fell(psel && penable && pready) |-> !pslverr; 
+  endproperty
+
+  property p4;
+    @(posedge clk) disable iff(!presetn)
+    paddr>= 2^32 |-> $rose(pslverr);
+  endproperty
+
+    property p5;
+    @(posedge clk) disable iff(!presetn)
+      $rose(penable) |-> (##[15]!pready && ##[16:$]pready) |-> $rose(pslverr);
+  endproperty
+  //or
+  property p5;
+    @(posedge clk) disable iff(!presetn)
+    $rose(penable) |-> (!pready[->15] && ##[16:$]pready) |-> $rose(pslverr);
+  endproperty
+
+  //after psel asserted then in next cycle penable asserted
+  property p6;
+    @(posedge clk) disable iff(!presetn)
+    $rose(psel) |=> $rose(penable);
+  endproperty
+
+  //if trying to write in a read_only memory(let say 4 is the RO location) 
+  property p7;
+    @(posedge pclk) disable iff(!presetn)
+    pwrite && padder[15:0]==16'b04 |-> $rose(pslverr);
+  endproperty
+
+  //if trying to read in a write_only memory(let say 4 is the WO location) 
+  property p7;
+    @(posedge pclk) disable iff(!presetn)
+    !pwrite && padder[15:0]==16'b08 |-> $rose(pslverr);
+  endproperty
+  
+  
+
   
